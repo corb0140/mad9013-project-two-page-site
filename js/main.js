0f5d731be5071031c06cbf6f50703d46121c21cb
Mark Corbin <corb0140@algonquinlive.com>
2023-11-03 11:01:34 -0400

    updated styling

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 // Hamburger menu and sidebar
 let icon = document.querySelector(".nav--hamburger-menu");
-let navMobile = document.querySelector(".header--nav-mobile");
+let navSidebar = document.querySelector(".header--nav-sidebar");
 let line = document.querySelectorAll(".nav--hamburger-menu__line");
 
 icon.addEventListener("click", function () {
-  if (navMobile.style.transform === `translateX(15em)`) {
+  if (navSidebar.style.transform === `translateX(15em)`) {
     line[0].style.transform = `rotate(0deg) translate(0, 0)`;
     line[1].style.width = `2em`;
     line[2].style.transform = `rotate(0deg) translate(0, 0)`;
-    navMobile.style.transform = `translateX(0em)`;
+    navSidebar.style.transform = `translateX(0em)`;
   } else {
     line[0].style.transform = `rotate(45deg) translate(0, .83em)`;
     line[1].style.width = `0`;
     line[2].style.transform = `rotate(-45deg) translate(0, -.83em)`;
-    navMobile.style.transform = `translateX(15em)`;
+    navSidebar.style.transform = `translateX(15em)`;
   }
 });
 
